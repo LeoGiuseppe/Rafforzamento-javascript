11cb3bb13df75d4c260f6fafbfda0a9abab7f4f8
LeoGiuseppe <leo.giuseppe@iisgalvanimi.edu.it>
2026-01-21 13:56:02 +0000

    incompleto punto 3

--- Esercizi_completi/contatore/contatore.js
+++ Esercizi_completi/contatore/contatore.js
@@ -1,7 +1,9 @@
-let contatore =Number(document.getElementById("testo"))
+let contatore =0
 function f1(){
-
+    contatore+=1
+document.getElementById("testo").textContent=contatore
 }
 function f2(){
-    
+      contatore-=1
+document.getElementById("testo").textContent=contatore
 }
