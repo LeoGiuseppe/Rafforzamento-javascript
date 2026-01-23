292310ad7d7af31b13d2e5bac1b747c9e5950479
LeoGiuseppe <leo.giuseppe@iisgalvanimi.edu.it>
2026-01-23 08:38:34 +0000

    punto 3

--- Esercizi_completi/contatore/contatore.js
+++ Esercizi_completi/contatore/contatore.js
@@ -1,9 +1,39 @@
 let contatore =0
+
+
 function f1(){
     contatore+=1
 document.getElementById("testo").textContent=contatore
+if (contatore==0) {
+    document.getElementById("testo").style.color="black"
+}
+else if(contatore<0){
+    document.getElementById("testo").style.color="red"
+}
+else if(contatore>0){
+    document.getElementById("testo").style.color="green"
+}
+ if(contatore>10){
+   document.getElementById("testo").style.fontWeight="bold"
+}else{
+     document.getElementById("testo").style.fontWeight="lighter"
+}
 }
 function f2(){
       contatore-=1
 document.getElementById("testo").textContent=contatore
+if (contatore==0) {
+    document.getElementById("testo").style.color="black"
+}
+else if(contatore<0){
+    document.getElementById("testo").style.color="red"
+}
+else if(contatore>0){
+    document.getElementById("testo").style.color="green"
+}
+ if(contatore>10){
+   document.getElementById("testo").style.fontWeight="bold"
+}else{
+     document.getElementById("testo").style.fontWeight="lighter"
+}
 }
