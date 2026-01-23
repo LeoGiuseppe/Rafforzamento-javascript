29af3ff5543a567026042738f416d5295c89b0c1
LeoGiuseppe <leo.giuseppe@iisgalvanimi.edu.it>
2026-01-23 09:59:46 +0000

    punto 4 incompleto

---
+++ Esercizi_completi/tabelline/tabelline.js
@@ -0,0 +1,15 @@
+let contatore=0
+let elemento=document.createElement("li");
+function f1() {
+ let lista = document.getElementById("listaDati"); // Prendi la lista "padre"
+
+
+for (let i = 0; i < 10; i++) {
+    
+    let elemento = document.createElement("li"); 
+    elemento.textContent = "Elemento numero " + i; 
+    lista.appendChild(nuovoPunto);                  
+  
+          
+}
+}
